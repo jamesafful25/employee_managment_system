038af2529c0838d78074f0edaa32e18f3730aa4a
James Afful <jamesafful25@users.noreply.github.com>
2026-06-08 22:53:05 +0000

    fix: fully corrected CI/CD pipeline (ECS + RDS + Terraform outputs)

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -17,3 +17,11 @@ output "rds_identifier" {
 output "db_secret_arn" {
   value = module.rds.db_secret_arn
 }
+
+output "private_subnets" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "ecs_security_group_id" {
+  value = module.ecs.security_group_id
+}
